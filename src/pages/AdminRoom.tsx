65b40bddd37eee650f686d4b197f152e9a092eb0
Gabriel Leocadio <gabrielsilva94@gmail.com>
2021-06-24 22:51:32 -0300

    feat: Aula 5 - Surface Exploration

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "react-hot-toast";
 
 import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg";
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
@@ -46,6 +48,18 @@ export function AdminRoom() {
         }
     }
 
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        });
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true,
+        });
+    }
+
     return (
         <div id="page-room">
             <header>
@@ -74,7 +88,38 @@ export function AdminRoom() {
                             key={question.id}
                             content={question.content}
                             author={question.author}
+                            isAnswered={question.isAnswered}
+                            isHighlighted={question.isHighlighted}
                         >
+                            {!question.isAnswered && (
+                                <>
+                                    <button
+                                        type="button"
+                                        onClick={() =>
+                                            handleCheckQuestionAsAnswered(
+                                                question.id
+                                            )
+                                        }
+                                    >
+                                        <img
+                                            src={checkImg}
+                                            alt="Marca pergunta como respondida"
+                                        />
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={() =>
+                                            handleHighlightQuestion(question.id)
+                                        }
+                                    >
+                                        <img
+                                            src={answerImg}
+                                            alt="Dar destaque à pergunta"
+                                        />
+                                    </button>
+                                </>
+                            )}
+
                             <button
                                 type="button"
                                 onClick={() =>
